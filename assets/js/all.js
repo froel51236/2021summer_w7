707f1c26a9f3ac3431a5e22aa03b9c4b19ac3c31
unknown <froel51234@gmail.com>
2021-08-24 18:41:35 +0800

    Update 2021-08-24T10:41:31.258Z

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+AOS.init({
+  offset: 150,
+  duration: 600
+});
+//# sourceMappingURL=all.js.map
